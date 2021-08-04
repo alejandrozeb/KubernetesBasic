49b3690b20f724b22c7afa6ebeeb2151abe231c8
unknown <alejandrojzeballos@gmail.com>
2021-08-04 18:56:18 -0400

    {add} kubernetes desafios de docker y aplicacion en kubernetes

--- apuntes.js
+++ apuntes.js
@@ -19,6 +19,38 @@
     clusteirng schedulling scaling
     load balancing fault tolerance appdeployment
 
+    -------Kubernetes a necessity-----------
+    ventajas de docker 
+    - los runtime pueden ser variaos y optmizados    
+    - tienen un empaquetado estandar con las imagenes que se usan en cualquier ambiente de desarrollo
+
+    luego tenemos a docker compose que ayuda a interconectar y administrar estos containers.
+
+    pero estas herramientas tienen un limite y es que solo lo hacen en una sola maquina en un docker host que puede ser tu pc.
+
+    cuando ya pasamos a produccion y tenemos una aplicacion con 100 o 500 containers ya se complica todo
+
+    por que en una infraestrucutra grande ya se usan nodos donde se alojan nuestros docker hosts entonces debemos afrontar alunos retos como
+
+    ---¿ QUE Y DONDE CORREN ESTOS CONTAINERS?
+    el problema a esta pregunta en la programacion el horario el scheduliing de cada container y tambien la escalabilidad.
+
+    ----¿COMO CONECTAR DOS DOCKER HOST, COMO CONECTAR DOS CONTAINERS EN DIFERENTES HOST?-----------
+    problemas  networking ademas como llevas a cabo el update de cada container con un nuevo feature ademas de los problemas al replicar los servicios que tambien lleva a la autoescalabilidad.
+
+    tambien el balanceo de los servicios ademas del acoplamiento de cada container eliminar containers pero que afecte a la aplicacion
+
+    ---------Solution--------------------
+    COE COntainer Orchestration Engine
+     called kubernetes
+
+     que se encarga de estos temas añadiendo conceptos que nos permitan manejar todo estos desafios.
+
+     cada nodo podemos crear una entidad logica llamada cluster pueden ser desde 2,5,8, hasta 5000 o mas. ademas se encarga de añadir nuvos nodos eliminarlos claro que las liitantes de estos cluster depende del servicio que se use por que finalmente el limite sera el hardware en este caso.
+
+
+
+
 
 
 
